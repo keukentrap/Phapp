a094b6ff51a50f48e7270bd4f217e0d305e7aaf0
Zwaardav <d.korsman@student.ru.nl>
2016-05-12 10:49:00 +0200

    Aangepast?

--- app/src/main/java/haakjeopenen/phapp/LoginActivity.java
+++ app/src/main/java/haakjeopenen/phapp/LoginActivity.java
@@ -3,6 +3,7 @@ package haakjeopenen.phapp;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -190,6 +191,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+
+            Intent intent = new Intent(this, Hoofdmenu.class);
+            startActivity(intent);
         }
     }
     private boolean isUsernameValid(String email) {
