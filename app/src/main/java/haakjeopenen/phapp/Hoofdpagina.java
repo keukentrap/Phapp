7111c291e60796412a6f4962dfaa629560ef1847
Caja Sieben <C.Sieben@hotmail.co.uk>
2016-05-12 13:57:21 +0200

    hoofdpagina

---
+++ app/src/main/java/haakjeopenen/phapp/Hoofdpagina.java
@@ -0,0 +1,13 @@
+package haakjeopenen.phapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hoofdpagina extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hoofdpagina);
+    }
+}
