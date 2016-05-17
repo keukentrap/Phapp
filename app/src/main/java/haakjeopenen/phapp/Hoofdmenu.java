46e1f0b80d008e5de6898092bcde0ba810b24868
Zwaardav <d.korsman@student.ru.nl>
2016-05-17 17:02:33 +0200

    Settings activity, uitlogknop die niet werkt

--- app/src/main/java/haakjeopenen/phapp/Hoofdmenu.java
+++ app/src/main/java/haakjeopenen/phapp/Hoofdmenu.java
@@ -69,6 +69,8 @@ public class Hoofdmenu extends AppCompatActivity
 
 		//noinspection SimplifiableIfStatement
 		if (id == R.id.action_settings) {
+			Intent intent = new Intent(this, SettingsActivity.class);
+			startActivity(intent);
 			return true;
 		}
 
