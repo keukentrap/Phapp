b573fa67c481f71e7fa54371b1fc50f906ea736c
Zwaardav <d.korsman@student.ru.nl>
2016-05-19 11:46:00 +0200

    Alles is waarschijnlijk helemaal kapot nu omdat Android Studio

--- app/src/main/java/haakjeopenen/phapp/DebugActivity.java
+++ app/src/main/java/haakjeopenen/phapp/DebugActivity.java
@@ -6,14 +6,25 @@ import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 // Kijken wat hiervan nodig is
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -25,6 +36,8 @@ public class DebugActivity extends AppCompatActivity {
 	Button submit;
 	TextView antwoord;
 	TextView responsecode;
+	Switch oudecodeswitch;
+	RequestQueue queue;
 
 	private final String USER_AGENT = "Phapp/0.0";
 
@@ -39,11 +52,32 @@ public class DebugActivity extends AppCompatActivity {
 		submit = (Button) findViewById(R.id.sendDebugGet);
 		antwoord = (TextView) findViewById(R.id.debugRequestAnswer);
 		responsecode = (TextView) findViewById(R.id.responseCodeView);
+		oudecodeswitch = (Switch) findViewById(R.id.oudecodeSwitch);
 
 		antwoord.setMovementMethod(new ScrollingMovementMethod());
+
+
+		// Instantiate the RequestQueue.
+		queue = Volley.newRequestQueue(this);
 	}
 
 	public void sendAPIgetRequest(View v) throws Exception
+	{
+		if (oudecodeswitch.isChecked())
+			oldget();
+		else
+			newget();
+	}
+
+	public void sendAPIpostRequest(View v) throws Exception
+	{
+		if (oudecodeswitch.isChecked())
+			oldpost();
+		else
+			newpost();
+	}
+
+	private void oldget() throws Exception
 	{
 		Thread thread = new Thread(new Runnable()
 		{
@@ -95,7 +129,7 @@ public class DebugActivity extends AppCompatActivity {
 		thread.start();
 	}
 
-	public void sendAPIpostRequest(View v) throws Exception
+	private void oldpost() throws Exception
 	{
 		Thread thread = new Thread(new Runnable()
 		{
@@ -155,5 +189,80 @@ public class DebugActivity extends AppCompatActivity {
 		});
 
 		thread.start();
+	}
+
+	private void newget()
+	{
+		String urlprefix = urltext.getText().toString();
+		String requeststring = requesttext.getText().toString();
+
+		String url = urlprefix + requeststring;
+
+		StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+				new Response.Listener<String>() {
+					@Override
+					public void onResponse(String response) {
+						// Display all of the response.
+						antwoord.setText(response);
+					}
+				},
+				new Response.ErrorListener() {
+					@Override
+					public void onErrorResponse(VolleyError error) {
+						antwoord.setText("Ño: " + error.toString() + ", " + String.valueOf(error.networkResponse.statusCode));
+						responsecode.setText(String.valueOf(error.networkResponse.statusCode));
+					}
+				}
+		);
+		queue.add(stringRequest);
+	}
+
+	private void newpost()
+	{
+		String urlprefix = urltext.getText().toString();
+		String requeststring = requesttext.getText().toString();
+
+		String url = urlprefix + requeststring;
+
+		StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+				new Response.Listener<String>() {
+					@Override
+					public void onResponse(String response) {
+						// Display all of the response.
+						antwoord.setText(response);
+					}
+				},
+				new Response.ErrorListener() {
+					@Override
+					public void onErrorResponse(VolleyError error) {
+						antwoord.setText("Ño: " + error.toString() + ", " + String.valueOf(error.networkResponse.statusCode));
+						responsecode.setText(String.valueOf(error.networkResponse.statusCode));
+					}
+				}
+		){
+			@Override
+			protected Map<String,String> getParams()
+			{
+				Map<String,String> params = new HashMap<String,String>();
+
+				// Best bagger eigenlijk
+				String[] split = requesttext2.getText().toString().split("&");
+				for (String s : split)
+				{
+					String[] splitparam = s.split("=");
+					params.put(splitparam[0], splitparam[1]);
+				}
+
+				return params;
+			}
+
+			@Override
+			public Map<String,String> getHeaders() throws AuthFailureError {
+				Map<String,String> params = new HashMap<String,String>();
+				params.put("Content-Type","appplication/x-www-form-urlencoded");
+				return params;
+			}
+		};
+		queue.add(stringRequest);
 	}
 }
