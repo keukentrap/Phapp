00f4c9fa450fdbc9c284fd431c584ffe203f804e
Wietze Mulder <wietzem@gmail.com>
2016-06-03 15:06:29 +0200

    Added meFragment (Still unused) Added RoundedImageView

---
+++ app/src/main/java/haakjeopenen/phapp/fragments/MeFragment.java
@@ -0,0 +1,36 @@
+package haakjeopenen.phapp.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import haakjeopenen.phapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MeFragment extends Fragment {
+
+
+    public MeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_me, container, false);
+    }
+
+}
