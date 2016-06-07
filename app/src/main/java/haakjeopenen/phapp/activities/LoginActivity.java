895a34cb7ef7df2d5658392742a29d0a4117e19b
Zwaardav <d.korsman@student.ru.nl>
2016-06-07 13:50:25 +0200

    Cleaned LoginActivity a bit, should still read it I guess

--- app/src/main/java/haakjeopenen/phapp/activities/LoginActivity.java
+++ app/src/main/java/haakjeopenen/phapp/activities/LoginActivity.java
@@ -24,12 +24,10 @@ import android.widget.TextView;
 import haakjeopenen.phapp.R;
 import haakjeopenen.phapp.net.API;
 
-//TODO David, clean this code please
-
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity { // implements LoaderCallbacks<Cursor>
+public class LoginActivity extends AppCompatActivity {
 
     private static final String PREFS_NAME = "Phapp_BasicLogin";
     private API api;
@@ -180,63 +178,6 @@ public class LoginActivity extends AppCompatActivity { // implements LoaderCallb
         }
     }
 
-    /*
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
-                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-                // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                                                                     .CONTENT_ITEM_TYPE},
-
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
-                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        List<String> emails = new ArrayList<>();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            emails.add(cursor.getString(ProfileQuery.ADDRESS));
-            cursor.moveToNext();
-        }
-
-        addEmailsToAutoComplete(emails);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-
-    }
-
-
-    private interface ProfileQuery {
-        String[] PROJECTION = {
-                ContactsContract.CommonDataKinds.Email.ADDRESS,
-                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-        };
-
-        int ADDRESS = 0;
-        int IS_PRIMARY = 1;
-    }
-
-
-    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(LoginActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-        mUsernameView.setAdapter(adapter);
-    }
-    */
-
     /**
      * Represents an asynchronous login/registration task used to authenticate
      * the user.
@@ -283,14 +224,8 @@ public class LoginActivity extends AppCompatActivity { // implements LoaderCallb
                 return true;
             } else {
                 System.out.println("invalid!");
-                //AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                //builder.setTitle(R.string.loginerror);
-                //builder.setMessage("Iets met wachtwoord ofzo");
-                //Looper.prepare();
-                //builder.create().show();
             }
 
-            // TO/DO: register the new account here.
             return false;
         }
 
